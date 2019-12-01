7823faee0bbbb7c86452e517d8f9d3c3f3f512f5
Matthew Barrington <matthew@barrington.it>
2019-12-01 11:38:19 +0000

    Day 1 part 2

--- src/one.rb
+++ src/one.rb
@@ -1,11 +1,32 @@
 #!/usr/bin/env ruby
 
-input = File.readlines("input/one.txt")
-
-total = 0
-
-input.each do |number|
-  total += (number.to_i / 3.0).floor - 2
+def required_fuel(mass)
+  (mass / 3.0).floor - 2
 end
 
-puts total
+def fuel_for_module(mass)
+  fuel = required_fuel(mass)
+
+  subtotal = fuel
+
+  until fuel <= 0
+    fuel = required_fuel(fuel)
+    subtotal += fuel if fuel.positive?
+  end
+
+  subtotal
+end
+
+def main
+  input = File.readlines("input/one.txt")
+
+  total = 0
+
+  input.each do |mass|
+    total += fuel_for_module(mass.to_i)
+  end
+
+  puts total
+end
+
+main
