2dcc7425f6ee7801cb361ff9f54415f99a91ce25
Matthew Barrington <matthew@barrington.it>
2019-12-05 00:10:09 +0000

    day 4 part 2

--- src/four.rb
+++ src/four.rb
@@ -1,9 +1,3 @@
-# It is a six-digit number.
-# The value is within the range given in your puzzle input.
-# Two adjacent digits are the same (like 22 in 122345).
-# Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
-require 'pry'
-
 def increasing?(numbers)
   (0...numbers.count-1).each do |i|
     return false if numbers[i] > numbers[i+1]
@@ -18,6 +12,15 @@ def adjacent?(numbers)
   false
 end
 
+def too_many?(numbers)
+  tmp = {}
+  numbers.each do |number|
+    tmp.key?(number) ? tmp[number] += 1 : tmp[number] = 1
+  end
+  return true if tmp.values.include?(2)
+  false
+end
+
 def main
   first = 137683
   last = 596253
@@ -26,15 +29,9 @@ def main
   (first..last).each do |password|
     split = password.to_s.chars.map(&:to_i)
 
-    tmp = {}
-    split.each do |number|
-      tmp.key?(number) ? tmp[number] += 1 : tmp[number] = 1
-    end
-
-    binding.pry
-
     next unless increasing?(split)
     next unless adjacent?(split)
+    next unless too_many?(split)
 
     passwords << split.join.to_i
   end
