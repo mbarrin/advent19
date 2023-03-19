a6f1a565e84c8cc5d1625fc911376c514d0349b1
Matthew Barrington <matthew@barrington.it>
2023-03-19 15:48:54 +0000

    Add in changed computer.go

--- go/internal/computer/computer.go
+++ go/internal/computer/computer.go
@@ -42,57 +42,81 @@ func (c *Computer) SetInputs(inputs []int) {
 	c.inputs = inputs
 }
 
-func (c *Computer) value(index int, mode byte) int {
+func (c *Computer) get(index int, mode byte) int {
 	if mode == 48 {
-		return c.commands[index]
+		return c.commands[c.commands[index]]
 	} else if mode == 49 {
-		return index
+		return c.commands[index]
 	} else if mode == 50 {
-		return c.r + c.commands[index]
+		return c.commands[c.r+c.commands[index]]
 	}
 	return 0
 }
 
-func (c *Computer) add(firstMode, secondMode byte) {
-	c.commands[c.commands[c.i+3]] = c.commands[c.value(c.i+1, firstMode)] + c.commands[c.value(c.i+2, secondMode)]
+func (c *Computer) set(index int, mode byte, value int) {
+	if mode == 48 {
+		c.commands[c.commands[index]] = value
+	} else if mode == 50 {
+		c.commands[c.r+c.commands[index]] = value
+	}
+}
+
+func (c *Computer) add(firstMode, secondMode, thirdMode byte) {
+	c.set(c.i+3, thirdMode, (c.get(c.i+1, firstMode) + c.get(c.i+2, secondMode)))
 	c.i += 3
 }
 
-func (c *Computer) mult(firstMode, secondMode byte) {
-	c.commands[c.commands[c.i+3]] = c.commands[c.value(c.i+1, firstMode)] * c.commands[c.value(c.i+2, secondMode)]
+func (c *Computer) mult(firstMode, secondMode, thirdMode byte) {
+	c.set(c.i+3, thirdMode, (c.get(c.i+1, firstMode) * c.get(c.i+2, secondMode)))
 	c.i += 3
+}
+
+func (c *Computer) read(firstMode byte) {
+	if len(c.inputs) == 0 {
+		c.inputs = append(c.inputs, <-c.inputChannel)
+	}
+	c.set(c.i+1, firstMode, c.inputs[0])
+	c.inputs = c.inputs[1:]
+	c.i += 1
+}
+
+func (c *Computer) write(firstMode byte) {
+	output := c.get(c.i+1, firstMode)
+	c.outputs = append(c.outputs, output)
+	c.outputChannel <- output
+	c.i += 1
 }
 
 func (c *Computer) jmp(firstmode, secondMode byte) {
-	if c.commands[c.value(c.i+1, firstmode)] != 0 {
-		c.i = c.commands[c.value(c.i+2, secondMode)] - 1
+	if c.get(c.i+1, firstmode) != 0 {
+		c.i = c.get(c.i+2, secondMode) - 1
 	} else {
 		c.i += 2
 	}
 }
 
 func (c *Computer) jne(firstmode, secondMode byte) {
-	if c.commands[c.value(c.i+1, firstmode)] == 0 {
-		c.i = c.commands[c.value(c.i+2, secondMode)] - 1
+	if c.get(c.i+1, firstmode) == 0 {
+		c.i = c.get(c.i+2, secondMode) - 1
 	} else {
 		c.i += 2
 	}
 }
 
-func (c *Computer) lessThan(firstmode, secondMode byte) {
-	if c.commands[c.value(c.i+1, firstmode)] < c.commands[c.value(c.i+2, secondMode)] {
-		c.commands[c.commands[c.i+3]] = 1
+func (c *Computer) lessThan(firstmode, secondMode, thirdMode byte) {
+	if c.get(c.i+1, firstmode) < c.get(c.i+2, secondMode) {
+		c.set(c.i+3, thirdMode, 1)
 	} else {
-		c.commands[c.commands[c.i+3]] = 0
+		c.set(c.i+3, thirdMode, 0)
 	}
 	c.i += 3
 }
 
-func (c *Computer) equals(firstmode, secondMode byte) {
-	if c.commands[c.value(c.i+1, firstmode)] == c.commands[c.value(c.i+2, secondMode)] {
-		c.commands[c.commands[c.i+3]] = 1
+func (c *Computer) equals(firstmode, secondMode, thirdMode byte) {
+	if c.get(c.i+1, firstmode) == c.get(c.i+2, secondMode) {
+		c.set(c.i+3, thirdMode, 1)
 	} else {
-		c.commands[c.commands[c.i+3]] = 0
+		c.set(c.i+3, thirdMode, 0)
 	}
 	c.i += 3
 }
@@ -100,66 +124,37 @@ func (c *Computer) equals(firstmode, secondMode byte) {
 func (c *Computer) Compute() int {
 	for {
 		// 0 pad to length 4 and split into an array
-		cmd := fmt.Sprintf("%04d", c.commands[c.i])
-		paramOneMode := cmd[1]
-		paramTwoMode := cmd[0]
+		cmd := fmt.Sprintf("%05d", c.commands[c.i])
+		paramOneMode, paramTwoMode, paramThreeMode := cmd[2], cmd[1], cmd[0]
 
 		// recreate the opcode
-		switch string(cmd[2:]) {
+		switch string(cmd[3:]) {
 		case "01":
-			// add
-			c.add(paramOneMode, paramTwoMode)
-
+			c.add(paramOneMode, paramTwoMode, paramThreeMode)
 		case "02":
-			// mult
-			c.mult(paramOneMode, paramTwoMode)
-
+			c.mult(paramOneMode, paramTwoMode, paramThreeMode)
 		case "03":
-			// read input
-			if len(c.inputs) == 0 {
-				c.inputs = append(c.inputs, <-c.inputChannel)
-			}
-			param1 := c.value(c.i+1, paramOneMode)
-			c.commands[param1] = c.inputs[0]
-			c.inputs = c.inputs[1:]
-			c.i += 1
-
+			c.read(paramOneMode)
 		case "04":
-			// write output
-			param1 := c.value(c.i+1, paramOneMode)
-			output := c.commands[param1]
-			c.outputs = append(c.outputs, output)
-			c.outputChannel <- output
-			c.i += 1
-
+			c.write(paramOneMode)
 		case "05":
-			// jump if true
 			c.jmp(paramOneMode, paramTwoMode)
-
 		case "06":
-			// jump if false
 			c.jne(paramOneMode, paramTwoMode)
-
 		case "07":
-			// less than
-			c.lessThan(paramOneMode, paramTwoMode)
-
+			c.lessThan(paramOneMode, paramTwoMode, paramThreeMode)
 		case "08":
-			// equal to
-			c.equals(paramOneMode, paramTwoMode)
-
+			c.equals(paramOneMode, paramTwoMode, paramThreeMode)
 		case "09":
 			// update r
-			c.r += c.value(c.i+1, paramOneMode)
+			c.r += c.get(c.i+1, paramOneMode)
 			c.i += 1
-
 		case "99":
 			if c.wg != nil {
 				c.wg.Done()
 			}
 			return 0
 		}
-
 		c.i++
 	}
 }
