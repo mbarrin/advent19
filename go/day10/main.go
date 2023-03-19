c5ac134d9fa932751e0a1f69ccfeb6dd3684913f
Matthew Barrington <matthew@barrington.it>
2023-03-19 19:04:05 +0000

    day 10 part 1 and some amount of part 2

--- go/day10/main.go
+++ go/day10/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -13,10 +14,19 @@ type asteroid struct {
 	colID float64
 }
 
+type base struct {
+	location asteroid
+	count    int
+	angles   map[float64]int
+}
+
 func main() {
 	asteroids := loadData("input.txt")
 
-	fmt.Println("part 1:", visible(asteroids))
+	base := visible(asteroids)
+	fmt.Println("part 1:", base.count)
+
+	fmt.Println("part 2:", destroy(asteroids, base))
 }
 
 func loadData(filename string) map[asteroid]bool {
@@ -40,8 +50,31 @@ func loadData(filename string) map[asteroid]bool {
 	return asteroids
 }
 
-func visible(asteroids map[asteroid]bool) int {
-	max := 0
+func destroy(asteroids map[asteroid]bool, base base) float64 {
+	sortedKeys := []float64{}
+	total := 0
+	for k := range base.angles {
+		sortedKeys = append(sortedKeys, k)
+	}
+	sort.Float64s(sortedKeys)
+
+	for _, a := range sortedKeys {
+		base.angles[a] -= 1
+		total++
+		if base.angles[a] == 0 {
+			delete(base.angles, a)
+		}
+
+		if total == 200 {
+			return a
+		}
+	}
+
+	return 0
+}
+
+func visible(asteroids map[asteroid]bool) base {
+	max := base{}
 	for a := range asteroids {
 		lines := map[float64]int{}
 		local := map[asteroid]bool{}
@@ -54,11 +87,14 @@ func visible(asteroids map[asteroid]bool) int {
 
 		for l := range local {
 			angle := math.Atan2((l.colID-a.colID), (l.rowID-a.rowID)) * (180 / math.Pi)
+			angle = math.Mod((angle + 360), 360)
 			lines[angle] += 1
 		}
 
-		if len(lines) > max {
-			max = len(lines)
+		if len(lines) > max.count {
+			max.location = a
+			max.count = len(lines)
+			max.angles = lines
 		}
 	}
 	return max
